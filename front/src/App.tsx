98e607a8e62f677adedb97426f1dd7025b7b47f7
takeru1205 <takeru0639418@gmail.com>
2023-05-01 22:25:41 +0900

    [update] add todo form

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,39 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 
-const baseURL = "http://host.docker.internal:8000";
-// const baseURL = "http://localhost:8000/todos";
-
+const baseURL = "http://localhost:8000/todos";
 
 type Todo = {
-  id: number;
+  id: string;
   title: string;
+  description: string;
   completed: boolean;
 };
 
-const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+type PostBody = {
+  title: string;
+  description: string;
+  completed: boolean;
+};
 
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        const response = await axios.get<Todo[]>(baseURL);
-        setTodos(response.data);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    };
+const App = () => {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [postBody, setPostBody] = React.useState<PostBody>({
+    title: "",
+    description: "",
+    completed: false,
+  });
 
-    fetchTodos();
+  const getTodos = async () => {
+    const response = await axios.get(baseURL);
+    setTodos(response.data);
+  };
+
+  const postTodo = async () => {
+    await axios.post(baseURL, postBody).then((response) => {
+      setTodos([...todos, response.data]);
+    });
+  };
+
+  const handleChanges = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPostBody({ ...postBody, [e.target.name]: e.target.value });
+  };
+
+  React.useEffect(() => {
+    getTodos();
   }, []);
 
   return (
     <div>
-      <h1>Todos</h1>
+      <h1>Todo List</h1>
+      <input
+        name="title"
+        value={postBody.title}
+        onChange={handleChanges}
+      /> <br />
+      <input
+        name="description"
+        value={postBody.description}
+        onChange={handleChanges}
+      />{" "}
+      <br />
+      <button onClick={postTodo}>Submit</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            {todo.title} - {todo.completed ? "Completed" : "Not Completed"}
+            <span style={{ paddingRight: "2rem" }}>{todo.title}</span>
+            <span style={{ paddingRight: "2rem" }}>{todo.description}</span>
           </li>
         ))}
       </ul>
@@ -42,4 +71,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
