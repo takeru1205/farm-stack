9d88ead5baa0e9d54ee4d41199e83e74937b1a6b
takeru1205 <takeru0639418@gmail.com>
2023-05-01 22:25:20 +0900

    [fix] apply CORS

--- api/main.py
+++ api/main.py
@@ -1,9 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from router.todo import router as todo_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(todo_router)
 
